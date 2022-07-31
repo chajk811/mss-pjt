9ff468aca883bf229a11a8f720a27652d3efa5f4
차진권 <chajinkwon@chajingwon-ui-MacBookPro.local>
2022-07-31 14:07:45 +0900

    filtering 추가

--- src/components/CharacterList.js
+++ src/components/CharacterList.js
@@ -2,21 +2,25 @@ import React, { useState, useEffect } from 'react'
 import qs from 'qs'
 import { getCharacters } from '../api/character/CharacterApi'
 
-const CharacterList = () => {
+const CharacterList = ({ filters }) => {
   const queryParams = qs.parse(window.location.search, { ignoreQueryPrefix: true })
   const _page = Number(queryParams.page) || 1
 
   const [characters, setCharacters] = useState([])
+  const [originList, setOriginList] = useState([])
   const [lastElement, setLastElement] = useState(null)
   const [page, setPage] = useState(_page)
 
-  console.log(lastElement, setLastElement)
+  // [TODO] filter on & fetchData 시, 필터링 적용
+  // [TODO] 삭제 버튼 기능 추가
+  // [TODO] 초기화 버튼 클릭 시, deleted data originList 로 초기화
 
-  const moreCharacters = () => {
+  const fetchData = () => {
     getCharacters(page)
       .then(res => {
         const { data } = res
         setCharacters(characters.concat(data))
+        setOriginList(originList.concat(data)) // save origin list data
       })
       .catch(error => console.log('error: ', error))
   }
@@ -30,8 +34,29 @@ const CharacterList = () => {
     })
   }
 
+  const filterData = () => {
+    let tmpData = originList // [TODO] 삭제한 데이터가 있는 경우, deleted data 불러오기
+
+    Object.entries(filters).forEach(([key, value]) => {
+      switch (value && key) {
+        case 'alive':
+          tmpData = tmpData.filter(v => v.isAlive)
+          break
+        case 'female':
+          tmpData = tmpData.filter(v => v.gender === 'Female')
+          break
+        case 'noTvSerises':
+          tmpData = tmpData.filter(v => v.tvSeries.filter(v => v).length === 0)
+          break
+      }
+    })
+
+    console.log('filtering Data: ', tmpData)
+    setCharacters(tmpData)
+  }
+
   useEffect(() => {
-    moreCharacters()
+    fetchData()
   }, [page])
 
   useEffect(() => {
@@ -43,6 +68,11 @@ const CharacterList = () => {
     }
     return () => observer && observer.disconnect()
   }, [lastElement])
+
+  useEffect(() => {
+    // filter btn 이 눌린 경우, 필터링
+    filterData()
+  }, [filters])
 
   return <div className='card-wrapper'>
     {characters.map((character, idx) => {
--- src/pages/MainContainer.js
+++ src/pages/MainContainer.js
@@ -1,19 +1,32 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './MainContainer.css'
 import CharacterList from '../components/CharacterList'
 
 const MainContainer = () => {
+  const [filters, setfilters] = useState({
+    alive: false,
+    female: false,
+    noTvSerises: false
+  })
+
+  const toggleFilter = (flag) => {
+    setfilters({
+      ...filters,
+      [flag]: !filters[flag]
+    })
+  }
+
   return <div className='container'>
     <div className='main'>
       <button className='top-btn'>top</button>
       <div className='title'>000 과제</div>
       <div className='filter-wrapper'>
-        <button className='active'>생존인물만</button>
-        <button>여자</button>
-        <button>tvSeries 없음</button>
+        <button className={filters.alive ? 'active' : ''} onClick={e => toggleFilter('alive')}>생존인물만</button>
+        <button className={filters.female ? 'active' : ''} onClick={e => toggleFilter('female')}>여자</button>
+        <button className={filters.noTvSerises ? 'active' : ''} onClick={e => toggleFilter('noTvSerises')}>tvSeries 없음</button>
         <button>초기화</button>
       </div>
-      <CharacterList />
+      <CharacterList filters={filters} />
     </div>
   </div>
 }
